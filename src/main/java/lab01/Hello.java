177be35d979a18b9da24d8d3aa097244981f8098
Gordon Rose <gar018@bucknell.edu>
2022-08-29 19:01:19 -0400

    lab01, Exercise 1, Complete

---
+++ src/main/java/lab01/Hello.java
@@ -0,0 +1,22 @@
+/* *****************************************
+*CSCI205 -Software Engineering and Design* Fall 2022
+* Instructor: Prof. King
+* Section: 01 - 9:00-9:50AM
+*
+* Name: Gordon Rose
+* Date: 08/28/2002
+* Lab / Assignment: lab01
+*
+* Description: a basic "hello world" java program
+*
+* *****************************************/
+
+package lab01;
+
+public class Hello
+{
+    public static void main(String[] args) {
+        System.out.println("Programming is not a spectator sport!");
+        System.exit(0);
+    }
+}
